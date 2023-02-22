c82641791ffc0ad6db102a87e4a0e730c7cb2382
Claire Nollet <clairenlet@gmail.com>
2023-02-22 18:31:53 +0100

    build: :green_heart: only dist content to be host

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-export const mail = {
-  to: 'mailto:user@interieur.gouv.fr?subject=Sujet&body=Corps du courriel',
-  label: 'Nous écrire',
-}
-
-export const sendEmail = () => {
-  window.location = mail.to
-}
